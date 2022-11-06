09f9681b7a527ef7c74a2b997a4b3624e970e8a2
Maksym_Botvin <botvinmaxim@gmail.com>
2022-11-06 23:24:23 +0200

    first commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.botvin;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String name = "Maksym";
+        String surname = "Botvin";
+        System.out.println(name + " " + surname);
+        
+    }
+}
